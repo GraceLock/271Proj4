037509717af7aa1d07626685e49db0c6401d46ed
evlenny <116751501+evlenny@users.noreply.github.com>
2024-04-03 10:26:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "usecase.cpp"
 
+int main() {
+    
+    BST<string, string> *bst = create_bst<string,string>("binhex.txt");
+
+    cout << "Enter a binary string: ";
+    string binaryInput;
+    cin >> binaryInput;
+
+    string hexOutput = convert<string,string>(bst, binaryInput);
+    cout << "Hexadecimal conversion: " << hexOutput << endl;
+
+    delete bst;
+    return 0;
+}
